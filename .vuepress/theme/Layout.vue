cdcfbb31e183f4000f0689023e884a32f597ee3d
Sam Parker <samcparker1999@gmail.com>
2020-06-28 16:21:28 +0100

    Added more support for Netlify

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -70,6 +70,16 @@
             }
         }
     }
+
+    if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
 </script>
 
 <style>
